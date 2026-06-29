199a123eb242f04dc17c19027aab2fde7b3fee24
Dami Daramola <damidaramola7@gmail.com>
2026-06-30 00:59:02 +0100

    add storage funcs

--- lib/storage.ts
+++ lib/storage.ts
@@ -15,12 +15,50 @@ export function getTrip(tripId: string): Trip | null {
 
 }
 
-export function saveTrip(trip:Trip):void{
+export function saveTrip(trip: Trip): void {
 
-   try {
-    localStorage.setItem(`trip-${trip.id}`, JSON.stringify(trip))
-   }
-   catch(error){
-    console.error("Failed to save trip",error)
-   }
+    try {
+        localStorage.setItem(`trip-${trip.id}`, JSON.stringify(trip))
+    }
+    catch (error) {
+        console.error("Failed to save trip", error)
+    }
+}
+
+
+export function deleteTrip(tripId: string): void {
+    try {
+        localStorage.removeItem(`trip-${tripId}`)
+        localStorage.removeItem(`tasks-${tripId}`)
+        localStorage.removeItem(`packing-${tripId}`)
+        localStorage.removeItem(`packingComplete-${tripId}`)
+    } catch (error) {
+        console.error("Failed to delete trip", error)
+    }
+
+
+
+}
+
+
+export function getAllTrips(): Trip[] | [] {
+    const trips = []
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i)
+
+        if (key?.startsWith('trip-')) {
+
+            const value = localStorage.getItem(key)
+            if (!value) {
+                continue
+
+            }
+
+            const trip = JSON.parse(value) as Trip
+
+            trips.push(trip)
+        }
+
+    }
+    return trips
 }
