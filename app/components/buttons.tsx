4f1c57575f159a37962f3cd9283e2dbf216d0018
Dami Daramola <damidaramola7@gmail.com>
2026-07-08 00:54:42 +0100

    add size to button interface

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -1,7 +1,20 @@
 interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  variant?: "primary" | "secondary" | "danger";
-  disabled?: boolean;
-  type?: "button" | "submit" | "reset";
+    children: React.ReactNode;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    variant?: "primary" | "secondary" | "danger";
+    disabled?: boolean;
+    type?: "button" | "submit" | "reset";
+    size?: "sm" | "md" | "lg"
+}
+
+
+export default function Button({ }: ButtonProps) {
+
+        const sizeStyles = {
+        sm: "text-sm p-1",
+        md: "text-base p-2",
+        lg: "text-lg p-3"
+    };
+
+    
 }
